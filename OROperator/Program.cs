3cbea95c60a4c4ade429ad139749b8d95263984d
Mart-Hannes Reinfeldt <mart-hannes.reinfeldt@tktk.ee>
2024-12-14 13:52:47 +0200

    add Week5LogicalOperators code

---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿// Math >= 90 OR Chemistry >= 90
+
+// math && chemistry || math && bilogy || bilogy && chemistry
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) ||(biology >= 90 && chemistry >= 90))
+{
+    Console.WriteLine("congratulation! you have been accepted!");
+}
+else
+{
+    Console.WriteLine("you cannot be accepted");
+}
